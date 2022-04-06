62bd6b70b8231820dc3ced13b4fb8a28c9dfc63a
ashlinaronin <ashlinaronin@gmail.com>
2022-04-05 22:39:39 -0700

    Integrate prompts and images

--- scripts/prompts.js
+++ scripts/prompts.js
@@ -1,10 +1,20 @@
-const getRandomPrompt = () =>
-  prompts[Math.floor(Math.random() * prompts.length)];
+const imageCount = 8;
+
+const getRandomImageSrc = () => {
+    const imageIndex = Math.floor(Math.random() * imageCount);
+    const adjustedIndex = (imageIndex + 1).toString().padStart(2, '0');  
+    return `./images/geomorph_${adjustedIndex}.jpg`
+};
+const getRandomPrompt = () => prompts[Math.floor(Math.random() * prompts.length)];
+
 
 const showRandomPrompt = () => {
   const prompt = getRandomPrompt();
+  const imageSrc = getRandomImageSrc();
+
   document.getElementById(
     "prompt-header"
   ).innerText = `${prompt.type} (${prompt.speed})`;
   document.getElementById("prompt-text").innerText = prompt.text;
+  document.getElementById("score-image").src = imageSrc;
 };
