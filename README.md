a3dbb651fc4ff83b467defbacace05b5a04eac38
ashlinaronin <ashlinaronin@gmail.com>
2022-04-08 20:12:34 -0700

    Wire up start button

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # erosion-synth
+
+
+socket events
+- score.start -> sent by controller to server to signal to start the timer
+- score.change -> sent by server to performers to change the image/prompt
--- server.js
+++ server.js
@@ -7,19 +7,30 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+let scoreChangeInterval;
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
+  socket.on("score.start", (msg) => {
+    console.log(
+      "received score.start, starting timer and sending initial score.change"
+    );
+    io.emit("score.change");
+    scoreChangeInterval = setInterval(() => {
+      console.log("sending score.change to all clients");
+      io.emit("score.change");
+    }, 10000);
+  });
+
+  socket.on("score.stop", (msg) => {
+    clearInterval(scoreChangeInterval);
+  });
 });
-
-
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
-  const interval = setInterval(() => {
-    console.log('sending score.change to all clients');
-    io.emit('score.change');
-  }, 10000);
 });
